bc6da32b8203e513c7be832d21bd464937ccab3a
Saber Mohamed <sabermed66@gmail.com>
2023-05-29 17:02:50 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,66 @@
+import React, { useRef } from "react";
+import "./App.css";
+
+import video from "./assets/video.mp4";
+import useVideoPlayer from "./hooks/useVideoPlayer";
+
+const App = () => {
+  const videoElement = useRef(null);
+  const {
+    playerState,
+    togglePlay,
+    handleOnTimeUpdate,
+    handleVideoProgress,
+    handleVideoSpeed,
+    toggleMute,
+  } = useVideoPlayer(videoElement);
+  return (
+    <div className="container">
+      <div className="video-wrapper">
+        <video
+          src={video}
+          ref={videoElement}
+          onTimeUpdate={handleOnTimeUpdate}
+          controls
+        />
+        {/* <div className="controls">
+          <div className="actions">
+            <button onClick={togglePlay}>
+              {!playerState.isPlaying ? (
+                <i className="bx bx-play">play</i>
+              ) : (
+                <i className="bx bx-pause">pause</i>
+              )}
+            </button>
+          </div>
+          <input
+            type="range"
+            min="0"
+            max="100"
+            value={playerState.progress}
+            onChange={(e) => handleVideoProgress(e)}
+          />
+          <select
+            className="velocity"
+            value={playerState.speed}
+            onChange={(e) => handleVideoSpeed(e)}
+          >
+            <option value="0.50">0.50x</option>
+            <option value="1">1x</option>
+            <option value="1.25">1.25x</option>
+            <option value="2">2x</option>
+          </select>
+          <button className="mute-btn" onClick={toggleMute}>
+            {!playerState.isMuted ? (
+              <i className="bx bxs-volume-full">full</i>
+            ) : (
+              <i className="bx bxs-volume-mute">mute</i>
+            )}
+          </button>
+        </div> */}
+      </div>
+    </div>
+  );
+};
+
+export default App;
